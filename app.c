5b9bb2e57a3bb273bee2e75daf822d156ac27024
ramses2099 <ramses2099@gmail.com>
2024-09-16 18:29:47 -0400

    latest change in readme file

--- app.c
+++ app.c
@@ -2,57 +2,103 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <conio.h>
 
-// pass arguments to reference min, max
-void minMax(int arr[], int len, int *min, int *max){
-    *min = arr[0];
-    *max = arr[0];
-    
-    for(int i = 1; i < len; i++){
-        if(arr[i] < *min)
-            *min = arr[i];
-        else if(arr[i] > *max)
-            *max = arr[i];
-    }
-}
+typedef struct Node{
+    int data;
+    struct Node *next;
+} Node;
 
-// function returns pointer
-int *findMid(int arr[], int len){
-    return &arr[len/2];
-}
+Node *linkedlist = NULL;
 
-// function returns the sum of elements in array using pointer
-int sumSeries(int arr[], int len){    
-    int sum = 0, *p;
-
-    for(p = &arr[0]; p <= &arr[len-1]; p++){
-        sum += *p;
-    }
-   
-    return sum;
-}
-
-// function reverses the order of elements in array using pointer
-void reversedSeries(int *arr, int len){
-    int temp = 0;
-    for (int i = 0; i < len/2; i++)
-    {
-        temp = arr[i];
-        arr[i] = arr[len - 1 - i];
-        arr[len - 1 - i] = temp;
-    }
-}
-
+void insert_end(int);
+void insert_beg(int);
+void ldelete(int);
+void display();
 
 int main() {
     system("cls");
-    int arr[] ={1, 7, 11, 12, 13, 14, 15, 17, 18, 19};
-    int len = sizeof(arr) / sizeof(arr[0]);
+    int val, n;
+        
+    do
+    {
+        printf("\n*************************MENU***************************\n ");
+        printf("\n1. Insert at the end");
+        printf("\n2. Insert at the beginning");
+        printf("\n3. Delete a node");
+        printf("\n4. Display");
+        printf("\n5. Exit");
+        printf("\nEnter your choice: ");
+        scanf("%d", &n);
 
-    int *p = &arr[0];
-    printf("%p\n", p);
+
+        switch (n)
+        {
+        case 1:
+            printf("\nEnter the value to insert: ");
+            scanf("%d", &val);
+            insert_end(val);
+            break;
+        
+        case 2:
+            printf("\nEnter the value to insert: ");
+            scanf("%d", &val);
+            insert_beg(val);
+            break;
+            
+        case 3:
+            printf("\nEnter the value to delete: ");
+            scanf("%d", &val);
+            ldelete(val);
+
+        case 4:
+            printf("\nDisplay all elements of linked list: ");            
+            display();
+
+        default:
+            break;
+        }
+
+
+
+    } while (n != 5);
     
+
+
 
     return 0;
  
+}
+
+
+void insert_end(int val)
+{
+    Node *temp = linkedlist;
+    Node *newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = val;
+    newNode->next = NULL;
+    if(linkedlist == NULL){
+        linkedlist = newNode;
+    }else{
+        while(temp->next!= NULL){
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+
+}
+void insert_beg(int val)
+{
+    Node *temp = linkedlist;
+    Node *newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = val;
+    newNode->next = linkedlist;
+    linkedlist = newNode;
+}
+void ldelete(int val)
+{
+
+}
+void display(){
+
 }
